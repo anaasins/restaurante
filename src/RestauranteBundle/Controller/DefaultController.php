7e401f2601c63afe0ffd9b6a66eafa52d6fa60e0
Ana Asins <asinsanna@gmail.com>
2017-12-13 09:57:06 +0100

    eliminar y editar con enlaces desde lista de tapas

--- src/RestauranteBundle/Controller/DefaultController.php
+++ src/RestauranteBundle/Controller/DefaultController.php
@@ -61,4 +61,38 @@ class DefaultController extends Controller
         $tapas = $repository->findOneById($id);
         return $this->render('RestauranteBundle:Default:mostrarId.html.twig', array('tapas'=>$tapas));
     }
+
+    /**
+     * @Route("/eliminarTapa/{id}", name="eliminarTapa")
+     */
+    public function eliminarTapaAction($id)
+    {
+      $db=$this->getDoctrine()->getManager();
+      $eliminar = $db ->getRepository(Tapas::class)->find($id);
+      $db->remove($eliminar);
+      $db->flush();
+        return $this->redirectToRoute('listaTapas');
+    }
+
+    /**
+     * @Route("/editarTapa/{id}", name="editarTapa")
+     */
+    public function editarTapaAction(Request $request, $id)
+    {
+      $tapa=$this->getDoctrine()->getRepository(Tapas::class)->find($id);
+
+      $form=$this->createForm(TapasType::class, $tapa);
+      $form->handleRequest($request);
+      if ($form->isSubmitted() && $form->isValid()) {
+
+         //$cerveza = $form->getData();
+         $em = $this->getDoctrine()->getManager();
+         $em->persist($tapa);
+         $em->flush();
+
+         return $this->redirectToRoute('listaTapas');
+       }
+
+      return $this->render('RestauranteBundle:Default:editarTapa.html.twig', array('form'=>$form->createView()));
+    }
 }
