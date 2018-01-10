62afe8326d8676cf53787db84871ad758ee657b5
Ana Asins <asinsanna@gmail.com>
2018-01-10 09:48:10 +0100

    ACTIVIDAD 6: http_basic, nuevo bundle(clientes)

---
+++ src/ClientesBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ClientesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ClientesBundle:Default:index.html.twig');
+    }
+}
